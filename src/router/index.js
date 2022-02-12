81d3393a6e492323f3150dbf4af569f32894c0f7
PaulSong213 <songaliajohnpaul@gmail.com>
2022-02-12 19:23:03 +0800

    fix infinite redirect

--- src/router/index.js
+++ src/router/index.js
@@ -1,116 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import route1 from './route1'
 const routes = [
   {
     path: '/introduction',
     name: 'Introduction',
     component: () => import('../components/Introduction.vue')
   },
-  {
-    path: '/css-max-min',
-    name: 'CSS Max Min Visualizer',
-    component: () => import('../components/CSSMaxMinVisualizer.vue')
-  },
-  {
-    path: '/oop-polymorphism-visualizer',
-    name: 'OOP Polymorphism Visualizer',
-    component: () => import('../components/OOPPolymorphismVisualizer.vue')
-  },
-  {
-    path: '/oop-encapsulation-visualizer',
-    name: 'OOP Encapsulation Visualizer',
-    component: () => import('../components/OOPEncapsulationVisualizer.vue')
-  },
-  {
-    path: '/css-absolute-relative-visualizer',
-    name: 'CSS Absolute Relative Visualizer',
-    component: () => import('../components/CSSAbsoluteRelativeVisualizer.vue')
-  },
-  {
-    path: '/js-timing-events-visualizer',
-    name: 'JS Timing Events Visualizer',
-    component: () => import('../components/JSTimingEventsVisualizer.vue')
-  },
-  {
-    path: '/js-array-method-visualizer',
-    name: 'JS Array Method Visualizer',
-    component: () => import('../components/JSArrayMethodVisualizer.vue')
-  },
-  {
-    path: '/css-adding-visualizer',
-    name: 'CSS Adding Visualizer',
-    component: () => import('../components/CSSAddingVisualizer.vue')
-  },
-  {
-    path: '/css-flexbox-visualizer',
-    name: 'CSS Flexbox Visualizer',
-    component: () => import('../components/CSSFlexboxVisualizer.vue')
-  },
-  {
-    path: '/css-overflow-visualizer',
-    name: 'CSS Overflow Visualizer',
-    component: () => import('../components/CSSOverflowVisualizer.vue')
-  },
-  {
-    path: '/css-pseudo-element',
-    name: 'CSS Pseudo Element',
-    component: () => import('../components/CSSPseudoElement.vue')
-  },
-  {
-    path: '/css-transform-visualizer',
-    name: 'CSS 2D Transform Visualizer',
-    component: () => import('../components/CSS2DTransformVisualizer.vue')
-  },
-  {
-    path: '/css-center-absolute-div',
-    name: 'CSS Center Absolute Div',
-    component: () => import('../components/CSSCenterAbsoluteDiv.vue')
-  },
-  {
-    path: '/js-async-visualizer',
-    name: 'Js Async Visualizer',
-    component: () => import('../components/JsAsyncVisualizer.vue')
-  },
-  {
-    path: '/text-icon-navbar',
-    name: 'Text Icon Navbar',
-    component: () => import('../components/TextIconNavbar.vue')
-  },
-  {
-    path: '/freelance-office',
-    name: 'Freelance Office',
-    component: () => import('../components/FreelanceOffice.vue')
-  },
-  {
-    path: '/photography',
-    name: 'Photography',
-    component: () => import('../components/Photography.vue')
-  },
-  {
-    path: '/email-notification',
-    name: 'Email Notification',
-    component: () => import('../components/EmailNotification.vue')
-  },
-  {
-    path: '/hero-page-Object',
-    name: 'Hero Page 3d Object',
-    component: () => import('../components/HeroPage3dObject.vue')
-  },
-  {
-    path: '/minimalist-notes',
-    name: 'Minimalist Notes',
-    component: () => import('../components/MinimalistNotes.vue')
-  },
-  {
-    path: '/user-experience-rate',
-    name: 'User Experience Rate',
-    component: () => import('../components/UserExperienceRate.vue')
-  },
-  {
-    path: '/collapsible-filters',
-    name: 'Collapsible Filters',
-    component: () => import('../components/CollapsibleFilters.vue')
-  },
+  ...route1,
   {
     path: '/slide-menu',
     name: 'Slide Menu',
