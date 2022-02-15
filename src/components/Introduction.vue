7a9c45529ccbd89091f89c0dbd286d12487c6651
PaulSong213 <songaliajohnpaul@gmail.com>
2022-02-15 18:43:37 +0800

    Add social media & logo on intrduction and  firebase analytics

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -2,9 +2,13 @@
   <section class="bg-green-500 h-screen flex flex-col justify-center">
 
     <div class="text-left sm:text-center text-green-100 font-bold px-5 max-w-lg mx-auto">
+      <div class="flex text-white space-x-3 mb-2 sm:mx-auto w-max">
+        <a href="/"><img class="w-12" src="/logo-white.png" alt="logo"></a>
+        <a href="https://www.tiktok.com/@webcomponents" target="_blank" class="text-4xl" ><ion-icon name="logo-tiktok"></ion-icon></a>
+      </div>
       <h1 class="text-4xl sm:text-3xl">This webpage composed of compilation of website components</h1>
-      <h5 class="font-normal text-gray-300 text-md my-5">Created by 
-        <a class="text-green-800 underline" href="https://www.songalia.me/">Paul</a>
+      <h5 class="font-normal text-gray-200 text-md my-5">Created with 🤍 by 
+        <a class="text-white font-bold underline" target="_blank" href="https://www.songalia.me/">Paul</a>
       </h5>
       <a class="border-b-4 border-green-300" href="/">View latest Project</a>
     </div>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import './index.css'
 import router from './router';
 createApp(App)
 .use(router)
 .mount('#app')
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
