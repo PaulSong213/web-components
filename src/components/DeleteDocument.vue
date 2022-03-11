8691ee6f91d20f59d7c29e3703fcb7114fc4f3c0
PaulSong213 <songaliajohnpaul@gmail.com>
2022-03-11 19:16:45 +0800

    Add delete document

--- src/components/DeleteDocument.vue
+++ src/components/DeleteDocument.vue
@@ -1,27 +1,27 @@
 <template>
-  <section class='flex flex-col justify-center h-screen px-2 bg-sky-400 relative overflow-hidden'>
-    <div class="w-56 h-64 rounded-2xl  bg-slate-800 absolute  z-10 left-1/2 -translate-x-1/2" :class="isDeleted ? ' transition-all duration-[3000ms] top-full' : '-top-72' ,'' " >
+  <section class='relative flex flex-col justify-center h-screen px-2 overflow-hidden bg-sky-400'>
+    <div class="absolute z-10 w-56 h-64 -translate-x-1/2 rounded-2xl bg-slate-800 left-1/2" :class="isDeleted ? ' transition-all duration-[3000ms] top-full' : '-top-72' ,'' " >
       <div class="relative w-full h-full px-4 py-8 space-y-3 opacity-50">
-        <div class="bg-white h-4 rounded-full w-5/6"></div>
-        <div class="bg-white h-4 rounded-full w-4/6"></div>
-        <div class="bg-white h-4 rounded-full w-4/6"></div>
-        <div class="bg-white h-4 rounded-full w-3/6"></div>
-        <div class="bg-white h-4 rounded-full w-4/6"></div>
-        <div class="bg-white h-4 rounded-full w-3/6"></div>
-        <div class="bg-white h-4 rounded-full w-5/6"></div>
-        <div class="bg-white h-4 rounded-full w-2/6"></div>
+        <div class="w-5/6 h-4 bg-white rounded-full"></div>
+        <div class="w-4/6 h-4 bg-white rounded-full"></div>
+        <div class="w-4/6 h-4 bg-white rounded-full"></div>
+        <div class="w-3/6 h-4 bg-white rounded-full"></div>
+        <div class="w-4/6 h-4 bg-white rounded-full"></div>
+        <div class="w-3/6 h-4 bg-white rounded-full"></div>
+        <div class="w-5/6 h-4 bg-white rounded-full"></div>
+        <div class="w-2/6 h-4 bg-white rounded-full"></div>
       </div>
     </div>
-    <button @click="isDeleted = !isDeleted" class="flex w-max mx-auto active:scale-90 transition-all bg-cyan-900 gap-1 z-40 rounded-lg">
-      <div class="bg-slate-900 w-64 h-24 mx-auto rounded-l-lg relative">
-        <h6 class="text-white/70 font-bold absolute left-4 text-xl top-1/2 -translate-y-1/2">Delete Document</h6>
+    <button @click="isDeleted = !isDeleted" class="z-40 flex gap-1 mx-auto transition-all rounded-lg w-max active:scale-90 bg-cyan-900">
+      <div class="relative w-64 h-24 mx-auto rounded-l-lg bg-slate-900">
+        <h6 class="absolute text-xl font-bold -translate-y-1/2 text-white/70 left-4 top-1/2">Delete Document</h6>
       </div>
-      <div class="h-24 bg-slate-900 rounded-r-lg w-12 relative">
-      <div class="w-4 h-4 rounded-full border-2 border-white bg-cyan-500 absolute left-4 top-1/2 -translate-y-1/2" :class="[isDeleted ? animate-pulse : '' ,'' ]" ></div>
+      <div class="relative w-12 h-24 rounded-r-lg bg-slate-900">
+      <div class="absolute w-4 h-4 -translate-y-1/2 border-2 border-white rounded-full bg-cyan-500 left-4 top-1/2" :class="[isDeleted ? animate-pulse : '' ,'' ]" ></div>
       </div>
     </button>
-    <div class=" w-72 h-1/2 absolute bottom-0 left-1/2 -translate-x-1/2 z-20">
-      <div class="relative w-full h-full flex gap-4">
+    <div class="absolute bottom-0 z-20 -translate-x-1/2 w-72 h-1/2 left-1/2">
+      <div class="relative flex w-full h-full gap-4">
         <div v-for="a in 10" class="w-4 h-full bg-sky-400"></div>
       </div>
     </div>
