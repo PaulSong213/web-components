841e64eb87a9cb4143774298a48394e43ea81806
PaulSong213 <songaliajohnpaul@gmail.com>
2022-04-03 15:25:42 +0800

    Add upwork link

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <section>
-    <div id="nav" v-if="showBottomNavigation" class=" absolute z-50 bg-green-500 bottom-10 right-10 rounded-full flex justify-between p-1 space-x-1 text-white text-5xl bg-opacity-50 shadow-xl border border-white/30">
-      <div v-if="currentIndex > 0" @click="moveToNextPath(false)" class="h-12  w-full rounded-l-full hover:bg-green-500 cursor-pointer active:bg-green-400 transition-all">
+    <div id="nav" v-if="showBottomNavigation" class="absolute z-50 flex justify-between p-1 space-x-1 text-5xl text-white bg-green-500 bg-opacity-50 border rounded-full shadow-xl bottom-10 right-10 border-white/30">
+      <div v-if="currentIndex > 0" @click="moveToNextPath(false)" class="w-full h-12 transition-all rounded-l-full cursor-pointer hover:bg-green-500 active:bg-green-400">
         <ion-icon class="pointer-events-none" name="caret-back-outline"></ion-icon>
       </div>
 
       <transition name="fade-ups">
-        <div v-if="currentIndex > 0" class="text-center text-base flex flex-col select-none my-auto">
+        <div v-if="currentIndex > 0" class="flex flex-col my-auto text-base text-center select-none">
           <span>{{currentIndex}}</span>
           <span class="table my-auto" style="font-size: 0.50rem;line-height: 0.75rem;">{{listPath.length - 1}} </span>
         </div>
       </transition>
-      <div @click="moveToNextPath(true)" class="h-12  w-full rounded-r-full hover:bg-green-500 cursor-pointer active:bg-green-400 transition-all">
+      <div @click="moveToNextPath(true)" class="w-full h-12 transition-all rounded-r-full cursor-pointer hover:bg-green-500 active:bg-green-400">
         <ion-icon class="pointer-events-none" name="caret-forward-outline"></ion-icon>
       </div>
     </div>
 
-    <a id="nav" v-if="showBottomNavigation" href="https://github.com/PaulSong213/web-components.git" class=" absolute z-50 bg-green-500 bottom-10 left-10 rounded-full flex justify-between py-2 px-4 space-x-1 text-white text-xs bg-opacity-50 shadow-sm border border-white/30 font-bold select-none">
-      Source Code
+    <a id="nav" v-if="!showBottomNavigation" href="https://github.com/PaulSong213/web-components.git" class="absolute z-50 flex justify-between px-4 py-2 space-x-1 text-xs font-bold text-center text-white bg-green-500 bg-opacity-50 border rounded-full shadow-sm select-none w-28 bottom-10 left-10 border-white/30">
+      <span class="m-auto">Source Code</span> 
+    </a>
+    <a id="nav" v-if="!showBottomNavigation" href="https://www.upwork.com/freelancers/~019c3e72e8b98317a9" class="absolute z-50 flex justify-between px-4 py-2 space-x-1 text-xs font-bold text-center text-white bg-green-500 bg-opacity-50 border rounded-full shadow-sm select-none bottom-20 left-10 border-white/30 w-28">
+      <span class="m-auto">Hire me</span>
     </a>
 
     <router-view v-slot="{ Component }">
